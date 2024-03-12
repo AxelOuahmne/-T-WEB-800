a41d310287be908db1d8003a963eb958caaf2ebe
ibguinathan@gmail.com <Nathan@0411>
2024-03-12 15:43:58 +0100

    [FIX]: 'BACKEND -> API'

--- backend/controllers/api.js
+++ backend/controllers/api.js
@@ -6,17 +6,29 @@ const clientSecret = config.CLIENT_SECRET;
 // Endpoint pour la fonctionnalité "sleep"
 exports.getApiSleep = async (req, res, next) => {
     try {
-        // Logique pour récupérer et renvoyer les hébergements disponibles
-                const currentDate = new Date().toISOString().slice(0, 10).replace(/-/g, ''); // Obtenez la date actuelle au format YYYYMMDD
+        // Vos identifiants Amadeus
+        const clientID = '6Y4vUsa5Ljk4F6Rfpi6jaaURyZNuE8m4'; // Remplacez par votre client ID Amadeus
+        const clientSecret = 'tXszT7d9Ur5Xlp4A'; // Remplacez par votre client secret Amadeus
 
-        const url = `https://api.foursquare.com/v3/places/search?query=Place%20de%20stalingrad%2C%20Paris%2C%2075019&sort=DISTANCE`;
+        // Obtenir le jeton d'accès
+        const tokenResponse = await axios.post('https://test.api.amadeus.com/v1/security/oauth2/token', `grant_type=client_credentials&client_id=${clientID}&client_secret=${clientSecret}`, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        // Extraire le jeton d'accès de la réponse
+        const accessToken = tokenResponse.data.access_token;
+
+        // Utiliser le jeton d'accès pour authentifier la requête vers l'API Amadeus pour rechercher des hôtels
+        const url = `https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city`;
 
         const response = await axios.get(url, {
             headers: {
-                'Accept': 'application/json',
-                'Authorization': clientSecret
+                'Authorization': `Bearer ${accessToken}`
             }
         });
+
 
         if (response.status === 200) {
             console.log(response.data); // Affiche la réponse de l'API dans la console
