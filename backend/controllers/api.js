bd9933368213df95c4fe1dea408d07f358a4f2fe
ibguinathan@gmail.com <Nathan@0411>
2024-03-26 11:56:19 +0100

    [FIX]: 'API Hotels'

--- backend/controllers/api.js
+++ backend/controllers/api.js
@@ -50,19 +50,12 @@ exports.getApiSleep = async (req, res, next) => {
 
 exports.getApiSleep2 = async (req, res, next) => {
     try {
-        console.log("la requete est dnen : ",req.body.location);
         // Vos identifiants Amadeus
-        const location = req.body.location;
         const clientID = '6Y4vUsa5Ljk4F6Rfpi6jaaURyZNuE8m4'; // Remplacez par votre client ID Amadeus
         const clientSecret = 'tXszT7d9Ur5Xlp4A'; // Remplacez par votre client secret Amadeus
+
         // Récupérer les données envoyées dans le corps de la requête POST
-        //const cityCode = req.body.destination;
-
-       /* if (!cityCode) {
-            return res.status(400).send('Le code de la ville est manquant dans la requête.');
-        }
-        */
-
+        const location = req.body.location;
 
         // Obtenir le jeton d'accès
         const tokenResponse = await axios.post('https://test.api.amadeus.com/v1/security/oauth2/token', `grant_type=client_credentials&client_id=${clientID}&client_secret=${clientSecret}`, {
@@ -102,18 +95,48 @@ exports.getApiSleep2 = async (req, res, next) => {
 }
 };
 
-
-
-// Endpoint pour la fonctionnalité "enjoy"
-exports.getApiEnjoy = async (req, res, next) => {
+exports.getApiHotelValidate = async (req, res, next) => {
     try {
-        // Logique pour récupérer et renvoyer les événements ou activités disponibles
-        const response = await axios.get('URL_API_POUR_ACTIVITÉS');
-        // Renvoyer les données récupérées en tant que réponse
-        res.json(response.data);
+        console.log("la requete est dnen : ", req.body.destination);
+
+        //Mes variables
+        const idHotel      = (req.body.idHotel) ? req.body.idHotel : 'MCLONGHM';
+        const nbAdult      = (req.body.nbAdult) ? req.body.nbAdult : 1;
+        const checkIn      = (req.body.checkIn) ? req.body.checkIn : '';
+        const checkOut     = (req.body.checkOut) ? req.body.checkOut : '';
+        const qttRoom      = (req.body.qttRoom) ? req.body.qttRoom : 1;
+
+
+
+        // Vos identifiants Amadeus
+        const clientID      = '6Y4vUsa5Ljk4F6Rfpi6jaaURyZNuE8m4'; // Remplacez par votre client ID Amadeus
+        const clientSecret  = 'tXszT7d9Ur5Xlp4A'; // Remplacez par votre client secret Amadeus
+        // Obtenir le jeton d'accès
+        const tokenResponse = await axios.post('https://test.api.amadeus.com/v1/security/oauth2/token', `grant_type=client_credentials&client_id=${clientID}&client_secret=${clientSecret}`, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        // Extraire le jeton d'accès de la réponse
+        const accessToken = tokenResponse.data.access_token;
+        // Utiliser le jeton d'accès pour authentifier la requête vers l'API Amadeus pour rechercher des hôtels
+        //const url           = `https://test.api.amadeus.com/v3/shopping/hotel-offers?hotelIds=${idHotel}&adults=${nbAdult}&checkInDate=${checkIn}&checkOutDate=${checkOut}&roomQuantity=${qttRoom}&paymentPolicy=NONE&bestRateOnly=true`;
+        const url             = `https://test.api.amadeus.com/v3/shopping/hotel-offers?hotelIds=MCLONGHM&adults=1&checkInDate=2024-03-26&checkOutDate=2024-03-27&roomQuantity=1&paymentPolicy=NONE&bestRateOnly=true`;
+        const response    = await axios.get(url, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+        if (response.status === 200) {
+            console.log(response.data); // Affiche la réponse de l'API dans la console
+            res.json(response.data);
+        } else {
+            console.error(`Erreur lors de la récupération des hébergements: ${response.statusText}`);
+            res.status(response.status).send('Erreur lors de la récupération des hébergements');
+        }
     } catch (error) {
         console.error(error);
-        res.status(500).send('Erreur lors de la récupération des activités'); // En cas d'erreur, renvoyer une réponse d'erreur
+        res.status(500).send('Erreur lors de la récupération des hébergements'); // En cas d'erreur, renvoyer une réponse d'erreur
     }
 };
 
