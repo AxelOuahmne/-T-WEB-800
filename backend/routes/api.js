564df7f85a9f81ff4d68dba3cdc38c6c4957ff67
ibguinathan@gmail.com <Nathan@0411>
2024-03-26 14:05:17 +0100

    [FIX]: 'API ROUTE Hotels'

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -5,6 +5,7 @@ const apiCtrl = require("../controllers/api");
 
 router.post("/sleep", apiCtrl.getApiSleep);
 router.post("/sleep2", apiCtrl.getApiSleep2);
+router.post("/sleep3", apiCtrl.getApiHotelValidate);
 router.get("/enjoy", apiCtrl.getApiEnjoy);
 router.post("/travel", apiCtrl.getApiTravel);
 router.get("/airport", apiCtrl.getApiAirport);
